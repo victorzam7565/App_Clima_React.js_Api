58202fa8bba1f480a75e386ba5ed9ee9f7a520c7
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-07-16 13:13:53 -0500

    clima

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Weather = () =>{
+   return (
+        <div className="container">
+          <div className="cards">
+            <h1>hola</h1>
+            <h5 className="py-4">
+              <i className="wi wi-day-sunny display-1"/>
+              </h5>
+            <h1 className="py-2">25&deg;</h1>
+
+            {/** mira la maxima y minima temp */}
+{minmaxTemp(24,19)}
+<h4 className="py-3">Slow Rain</h4>
+        </div>
+</div>
+   );
+};
+
+function minmaxTemp(min,max){
+  return(
+    <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+    </h3>
+
+  );
+}
+ export default Weather;
